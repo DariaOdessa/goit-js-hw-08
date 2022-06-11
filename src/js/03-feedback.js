7d8e6401cee01dffb13e58870131aebcf4653b5a
DariaOdessa <dariaonlynew@gmail.com>
2022-06-11 23:54:16 +0300

    add task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+
+const FEEDBACK_KEY = 'feedback-form-state';
+
+const formRef = document.querySelector('.feedback-form');
+const textareaRef = document.querySelector('.feedback-form textarea');
+const emailInputRef = document.querySelector('.feedback-form input')
+
+
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onFormInput, 500));
+
+const formData = {};
+
+getLocalStorageData();
+
+function onFormInput(evt) {
+
+    formData[evt.target.name] = evt.target.value;
+    const localStorageData = JSON.stringify(formData);
+    localStorage.setItem(FEEDBACK_KEY, localStorageData); 
+}
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(formData);
+    evt.currentTarget.reset();
+    localStorage.removeItem(FEEDBACK_KEY);
+}
+
+function getLocalStorageData() {
+    const savedData = localStorage.getItem(FEEDBACK_KEY);
+    const parsedData = JSON.parse(savedData);
+
+  if (savedData) {
+    emailInputRef.value = parsedData.email;
+    textareaRef.value = parsedData.message;
+  }
+}
+
+
 
